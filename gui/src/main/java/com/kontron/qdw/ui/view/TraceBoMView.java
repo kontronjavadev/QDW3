b882a29cd90a13c6ef652762a43bdba76f92f4d0
Raymund Achner <raymund.achner@achner.com>
2026-01-12 15:38:41 +0100

    code: TraceBoM-View, Teil 2: übliche Anpassungen

--- gui/src/main/java/com/kontron/qdw/ui/view/TraceBoMView.java
+++ gui/src/main/java/com/kontron/qdw/ui/view/TraceBoMView.java
@@ -17,6 +17,8 @@ import org.slf4j.*;
 import com.kontron.qdw.boundary.material.*;
 import java.lang.invoke.*;
 import com.kontron.qdw.ui.dialog.*;
+import com.kontron.qdw.ui.view.util.SuperView;
+
 import net.sourceforge.jbizmo.commons.webclient.primefaces.util.*;
 import jakarta.faces.application.FacesMessage;
 import java.util.*;
@@ -24,11 +26,13 @@ import jakarta.faces.view.*;
 import com.kontron.qdw.ui.*;
 import com.kontron.qdw.service.*;
 import com.kontron.qdw.dto.material.*;
+
+import net.sourceforge.jbizmo.commons.annotation.Customized;
 import net.sourceforge.jbizmo.commons.annotation.Generated;
 
 @Named("traceBoMView")
 @ViewScoped
-public class TraceBoMView extends AbstractSearchableView implements Serializable {
+public class TraceBoMView extends SuperView implements Serializable {
     @Generated
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     @Generated
@@ -92,6 +96,171 @@ public class TraceBoMView extends AbstractSearchableView implements Serializable
         this.supplierService = supplierService;
         this.queryManager = queryManager;
     }
+
+
+
+    /**
+     * Initialize view
+     */
+    @Customized
+    public void initView() {
+        logger.debug("Initialize view");
+
+        bundle = ResourceBundle.getBundle(DEFAULT_BUNDLE_NAME, userSession.getLocale());
+
+        // Check if user is allowed to open this page!
+        if (!userSession.checkAuthorization(true, ROLE_ADMINISTRATOR, ROLE_READONLY)) {
+            return;
+        }
+
+
+        formTitle = bundle.getString(FORM_TRACEBOMVIEW_TITLE);
+
+        if (searchObj == null) {
+            // Check if previous search exists!
+            final SearchDTO lastSearch = queryManager.getLastQuery(userSession.getPrincipal().getId(), VIEW_ID);
+            if (lastSearch != null) {
+                searchObj = lastSearch;
+                prepareAfterLoad();
+            }
+            else {
+                initSearchObject();
+            }
+        }
+
+        initProperties();
+        fetchTraceBoMs();
+
+        logger.debug("View initialization finished");
+    }
+
+    /**
+     * Initialize search object
+     */
+    @Customized
+    public void initSearchObject() {
+        searchObj = new SearchDTO();
+        int colOrderId = -1;
+
+        // Initialize search object
+        searchObj.setMaxResult(1000);
+        searchObj.setExactFilterMatch(true);
+        searchObj.setCaseSensitive(true);
+        searchObj.setCount(false);
+
+        refreshFormatSettings();
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALNUMBER,
+                bundle.getString(COL_TRACEBOMVIEW_MATREVMATMATERIALNUMBER), SearchFieldDataTypeEnum.STRING, 150);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATSAPNUMBER,
+                bundle.getString(COL_TRACEBOMVIEW_MATREVMATSAPNUMBER), SearchFieldDataTypeEnum.STRING, 150);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATSHORTTEXT,
+                bundle.getString(COL_TRACEBOMVIEW_MATREVMATSHORTTEXT), SearchFieldDataTypeEnum.STRING, 250);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALTYPECODE,
+                bundle.getString(COL_TRACEBOMVIEW_MATREVMATMATERIALTYPECODE), SearchFieldDataTypeEnum.STRING, 80);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALCLASSCODE,
+                bundle.getString(COL_TRACEBOMVIEW_MATREVMATMATERIALCLASSCODE), SearchFieldDataTypeEnum.STRING, 120);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALHIERARCHY,
+                bundle.getString(LBL_ATTR_MATERIAL_MATERIALHIERARCHY), SearchFieldDataTypeEnum.STRING, 150);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVREVISIONNUMBER,
+                bundle.getString(COL_TRACEBOMVIEW_MATREVREVISIONNUMBER), SearchFieldDataTypeEnum.STRING, 150);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_SUPPLIERNAME,
+                bundle.getString(COL_TRACEBOMVIEW_SUPPLIERNAME), SearchFieldDataTypeEnum.STRING, 200);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_PRODUCTIONDATE,
+                bundle.getString(COL_TRACEBOMVIEW_PRODUCTIONDATE), SearchFieldDataTypeEnum.LOCAL_DATE, 80, false);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_DELIVERYNOTENUMBER,
+                bundle.getString(COL_TRACEBOMVIEW_DELIVERYNOTENUMBER), SearchFieldDataTypeEnum.STRING, 100);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_ORDERNUMBER,
+                bundle.getString(COL_TRACEBOMVIEW_ORDERNUMBER), SearchFieldDataTypeEnum.STRING, 100);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_LOTNUMBER,
+                bundle.getString(COL_TRACEBOMVIEW_LOTNUMBER), SearchFieldDataTypeEnum.STRING, 80);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_CREATIONDATE,
+                bundle.getString(LBL_ATTR_ABSTRACTENTITYWITHID_CREATIONDATE), SearchFieldDataTypeEnum.LOCAL_DATE_TIME, 120);
+
+        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_LASTUPDATE,
+                bundle.getString(LBL_ATTR_ABSTRACTENTITYWITHID_LASTUPDATE), SearchFieldDataTypeEnum.LOCAL_DATE_TIME, 120);
+
+
+        visibleFields = new DualListModel<>();
+        visibleFields.setSource(new ArrayList<>());
+        visibleFields.setTarget(new ArrayList<>());
+
+        for (final SearchFieldDTO d : searchObj.getSearchFields()) {
+            if (!d.isVisible()) {
+                visibleFields.getSource().add(d);
+            }
+            else {
+                visibleFields.getTarget().add(d);
+            }
+        }
+    }
+
+    /**
+     * Perform data fetch operation
+     */
+    @Customized
+    public void fetchTraceBoMs() {
+        logger.debug("Perform data fetch operation");
+
+        try {
+            preSearch();
+        }
+        catch (final SearchInputFieldValidationException e) {
+            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_INFO, SEARCH_INPUT_VALIDATION, "", e.getSearchFieldName());
+            return;
+        }
+
+        refreshFormatSettings();
+        setCountFilterDependent(2);
+
+        try {
+            traceBoMsList = traceBoMService.searchAllTraceBoMs(searchObj);
+
+            if (searchObj.isCount()) {
+                if (traceBoMsList.size() == searchObj.getMaxResult()) {
+                    countResult = traceBoMService.countAllTraceBoMs(searchObj);
+                }
+                else {
+                    countResult = traceBoMsList.size();
+                }
+            }
+
+            queryManager.saveQuery(userSession.getPrincipal().getId(), VIEW_ID, null, searchObj);
+        }
+        catch (final Exception e) {
+            logger.error("Error while fetching data!", e);
+
+            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_ERROR, OPERATION_FETCH_FAIL, e);
+        }
+        finally {
+            postSearch();
+        }
+    }
+
+    @Override
+    protected String getViewName() {
+        return VIEW_ID;
+    }
+
+    @Override
+    public void resetSearchObject() {
+        initSearchObject();
+        fetchTraceBoMs();
+    }
+
+
 
     /**
      * @return the list of elements
@@ -225,6 +394,7 @@ public class TraceBoMView extends AbstractSearchableView implements Serializable
     /**
      * @return the name of the selected saved query
      */
+    @Override
     @Generated
     public String getSelectedSavedQuery() {
         return selectedSavedQuery;
@@ -256,141 +426,6 @@ public class TraceBoMView extends AbstractSearchableView implements Serializable
         searchObj.setNumberFormat(userSession.getNumberFormat());
         searchObj.setDecimalSeparator(DecimalFormatSymbols.getInstance(userSession.getLocale()).getDecimalSeparator());
         searchObj.setGroupingSeparator(DecimalFormatSymbols.getInstance(userSession.getLocale()).getGroupingSeparator());
-    }
-
-    /**
-     * Initialize search object
-     */
-    @Generated
-    public void initSearchObject() {
-        searchObj = new SearchDTO();
-        int colOrderId = -1;
-
-        // Initialize search object
-        searchObj.setMaxResult(1000);
-        searchObj.setExactFilterMatch(true);
-        searchObj.setCaseSensitive(false);
-        searchObj.setCount(false);
-
-        refreshFormatSettings();
-
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALNUMBER,
-                bundle.getString(COL_TRACEBOMVIEW_MATREVMATMATERIALNUMBER), SearchFieldDataTypeEnum.STRING, 150);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATSAPNUMBER,
-                bundle.getString(COL_TRACEBOMVIEW_MATREVMATSAPNUMBER), SearchFieldDataTypeEnum.STRING, 150);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATSHORTTEXT,
-                bundle.getString(COL_TRACEBOMVIEW_MATREVMATSHORTTEXT), SearchFieldDataTypeEnum.STRING, 250);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALTYPECODE,
-                bundle.getString(COL_TRACEBOMVIEW_MATREVMATMATERIALTYPECODE), SearchFieldDataTypeEnum.STRING, 80);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALCLASSCODE,
-                bundle.getString(COL_TRACEBOMVIEW_MATREVMATMATERIALCLASSCODE), SearchFieldDataTypeEnum.STRING, 120);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVMATMATERIALHIERARCHY,
-                bundle.getString(LBL_ATTR_MATERIAL_MATERIALHIERARCHY), SearchFieldDataTypeEnum.STRING, 150);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_MATREVREVISIONNUMBER,
-                bundle.getString(COL_TRACEBOMVIEW_MATREVREVISIONNUMBER), SearchFieldDataTypeEnum.STRING, 150);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_SUPPLIERNAME, bundle.getString(COL_TRACEBOMVIEW_SUPPLIERNAME),
-                SearchFieldDataTypeEnum.STRING, 200);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_PRODUCTIONDATE, bundle.getString(COL_TRACEBOMVIEW_PRODUCTIONDATE),
-                SearchFieldDataTypeEnum.LOCAL_DATE, 80, false);
-        new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_DELIVERYNOTENUMBER,
-                bundle.getString(COL_TRACEBOMVIEW_DELIVERYNOTENUMBER), SearchFieldDataTypeEnum.STRING, 100);
-
-        final var f11 = new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_ORDERNUMBER,
-                bundle.getString(COL_TRACEBOMVIEW_ORDERNUMBER), SearchFieldDataTypeEnum.STRING, 100);
-        f11.setVisible(false);
-
-
-        final var f12 = new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_LOTNUMBER,
-                bundle.getString(COL_TRACEBOMVIEW_LOTNUMBER), SearchFieldDataTypeEnum.STRING, 80);
-        f12.setVisible(false);
-
-
-        final var f13 = new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_CREATIONDATE,
-                bundle.getString(LBL_ATTR_ABSTRACTENTITYWITHID_CREATIONDATE), SearchFieldDataTypeEnum.LOCAL_DATE_TIME, 120);
-        f13.setVisible(false);
-
-
-        final var f14 = new JSFSearchFieldDTO(searchObj, ++colOrderId, TraceBoMSearchDTO.SELECT_LASTUPDATE,
-                bundle.getString(LBL_ATTR_ABSTRACTENTITYWITHID_LASTUPDATE), SearchFieldDataTypeEnum.LOCAL_DATE_TIME, 120);
-        f14.setVisible(false);
-
-
-        visibleFields = new DualListModel<>();
-        visibleFields.setSource(new ArrayList<>());
-        visibleFields.setTarget(new ArrayList<>());
-
-        for (final SearchFieldDTO d : searchObj.getSearchFields())
-            if (!d.isVisible())
-                visibleFields.getSource().add(d);
-            else
-                visibleFields.getTarget().add(d);
-    }
-
-    /**
-     * Initialize view
-     */
-    @Generated
-    public void initView() {
-        logger.debug("Initialize view");
-
-        bundle = ResourceBundle.getBundle(DEFAULT_BUNDLE_NAME, userSession.getLocale());
-
-        // Check if user is allowed to open this page!
-        if (!userSession.checkAuthorization(true, ROLE_ADMINISTRATOR, ROLE_READONLY))
-            return;
-
-
-        formTitle = bundle.getString(FORM_TRACEBOMVIEW_TITLE);
-
-        // Check if previous search exists!
-        final SearchDTO lastSearch = queryManager.getLastQuery(userSession.getPrincipal().getId(), VIEW_ID);
-
-        if (lastSearch != null) {
-            searchObj = lastSearch;
-
-            prepareAfterLoad();
-        }
-        else
-            initSearchObject();
-
-        fetchTraceBoMs();
-
-        logger.debug("View initialization finished");
-    }
-
-    /**
-     * Perform data fetch operation
-     */
-    @Generated
-    public void fetchTraceBoMs() {
-        logger.debug("Perform data fetch operation");
-
-        try {
-            preSearch();
-        }
-        catch (final SearchInputFieldValidationException e) {
-            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_INFO, SEARCH_INPUT_VALIDATION, "", e.getSearchFieldName());
-            return;
-        }
-
-        refreshFormatSettings();
-
-        try {
-            traceBoMsList = traceBoMService.searchAllTraceBoMs(searchObj);
-
-            if (searchObj.isCount())
-                countResult = traceBoMService.countAllTraceBoMs(searchObj);
-
-            queryManager.saveQuery(userSession.getPrincipal().getId(), VIEW_ID, null, searchObj);
-        }
-        catch (final Exception e) {
-            logger.error("Error while fetching data!", e);
-
-            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_ERROR, OPERATION_FETCH_FAIL, e);
-        }
-        finally {
-            postSearch();
-        }
     }
 
     /**
