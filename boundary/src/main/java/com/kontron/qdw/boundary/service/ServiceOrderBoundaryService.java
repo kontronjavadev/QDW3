51d78371448ad01b7d003cb4a9a6f2cded3be4cb
Raymund Achner <raymund.achner@achner.com>
2026-05-28 13:28:01 +0200

    code: ServiceOrderServiceMessagePanel (service messages für service order, bzw. Panel "Service orders" in "View RMA"), Teil 1

--- boundary/src/main/java/com/kontron/qdw/boundary/service/ServiceOrderBoundaryService.java
+++ boundary/src/main/java/com/kontron/qdw/boundary/service/ServiceOrderBoundaryService.java
@@ -2,6 +2,7 @@ package com.kontron.qdw.boundary.service;
 
 import com.kontron.qdw.domain.service.*;
 import net.sourceforge.jbizmo.commons.search.exception.*;
+import static net.sourceforge.jbizmo.commons.jpa.AbstractRepository.DEFAULT_LIST_SIZE;
 import static net.sourceforge.jbizmo.commons.jpa.AbstractRepository.WILDCARD;
 import com.kontron.qdw.dto.base.*;
 import com.kontron.qdw.dto.service.*;
@@ -106,6 +107,72 @@ public class ServiceOrderBoundaryService {
         }
 
         return repository.search(searchObj, ServiceOrderListDTO.class, selectTokens);
+    }
+
+    /**
+     * Get all service messages of a given service order
+     * @param id
+     * @return a list of service message objects
+     * @throws GeneralSearchException if the search operation has failed
+     */
+    @Generated
+    @PermitAll
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public List<ServiceOrderServiceMessagesDTO> getServiceMessagesOfServiceOrder(String id) {
+        // Collect the select tokens of all fields that should be fetched
+        final var selectTokens = new ArrayList<String>();
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_ANALYSISTEXT);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_BASICFINISHDATE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_BASICSTARTDATE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_CAUSETEXT);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_CUSTOMERFAILURE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_DEFECTCOMPONENT);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_DELIVERYNOTENUMBER);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_DESIGNATOR);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_EPIDEMICFAILURE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_ERRORID);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_INTERNALARRIVALDATE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_INTERNALSHIPMENTDATE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_ORIGIN);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRDESCRIPTION);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_SERVICEMESSAGEID);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_ID);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_CREATIONDATE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_LASTUPDATE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_PLANTCODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_SERVICEORDERCODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRSTATECODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_MATERIALREVISIONID);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_SERIALOBJECTID);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_SERIALOBJECTSERIALNUMBER);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_MATREVMATERIALID);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_MATREVMATMATERIALNUMBER);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_MATREVMATSAPNUMBER);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_MATREVREVISIONNUMBER);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_RMATYPECODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRSERVICECODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRLOCATIONCODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRERRORCODECODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRERRORCODEGROUPNAME);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRTASKCODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_REPAIRTASKSHORTTEXT);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_FAULTANALYSISCODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_FAULTANALYSISSHORTTEXT);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_EXTERNALSUPPLIERCODE);
+        selectTokens.add(ServiceOrderServiceMessagesDTO.SELECT_EXTERNALSUPPLIERNAME);
+
+        // Initialize the search object
+        final var searchObj = new SearchDTO();
+        searchObj.setExactFilterMatch(true);
+        searchObj.setCaseSensitive(true);
+        searchObj.setMaxResult(DEFAULT_LIST_SIZE);
+        searchObj.setFromClause(
+                "from ServiceOrder x join x.serviceMessages a left join a.externalSupplier b left join a.materialRevision c left join a.plant d left join a.serialObject e left join a.serviceOrder f left join a.faultAnalysis h left join a.rMAType i left join a.repairErrorCode j left join a.repairLocation k left join a.repairService l left join a.repairState m left join a.repairTask n left join c.material u");
+
+        final var parentFilterField = searchObj.addSearchField("x.code", SearchFieldDataTypeEnum.STRING);
+        parentFilterField.setFilterCriteria(id);
+
+        return repository.search(searchObj, ServiceOrderServiceMessagesDTO.class, selectTokens);
     }
 
 }
---
+++ gui/src/main/java/com/kontron/qdw/ui/panel/ServiceOrderServiceMessagesPanel.java
@@ -0,0 +1,237 @@
+package com.kontron.qdw.ui.panel;
+
+import org.slf4j.*;
+import java.lang.invoke.*;
+import com.kontron.qdw.ui.dialog.*;
+import static com.kontron.qdw.ui.TranslationKeys.*;
+import net.sourceforge.jbizmo.commons.webclient.primefaces.util.*;
+import com.kontron.qdw.boundary.service.*;
+import com.kontron.qdw.dto.service.*;
+import jakarta.faces.application.FacesMessage;
+import java.util.*;
+import jakarta.faces.view.*;
+import static com.kontron.qdw.ui.UserSession.*;
+import com.kontron.qdw.ui.*;
+import jakarta.inject.*;
+import net.sourceforge.jbizmo.commons.annotation.Generated;
+import java.io.*;
+
+@Named("serviceOrderServiceMessagesPanel")
+@ViewScoped
+public class ServiceOrderServiceMessagesPanel implements Serializable {
+    @Generated
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    @Generated
+    private static final long serialVersionUID = 1L;
+    @Generated
+    private List<ServiceOrderServiceMessagesDTO> serviceMessagesList = new ArrayList<>();
+    @Generated
+    private ServiceOrderServiceMessagesDTO selItemOfServiceMessages;
+    @Generated
+    private final UserSession userSession;
+    @Generated
+    private transient ResourceBundle bundle;
+    @Generated
+    private String selectedObjectId;
+    @Generated
+    private String currentPageURL;
+    @Generated
+    private boolean readOnly;
+    @Generated
+    private final transient ServiceOrderBoundaryService serviceOrderService;
+    @Generated
+    private final transient ServiceMessageBoundaryService serviceMessageService;
+
+    /**
+     * Default constructor
+     */
+    @Generated
+    public ServiceOrderServiceMessagesPanel() {
+        this.userSession = null;
+        this.serviceOrderService = null;
+        this.serviceMessageService = null;
+    }
+
+    /**
+     * Constructor for injecting all required beans
+     * @param userSession
+     * @param serviceOrderService
+     * @param serviceMessageService
+     */
+    @Inject
+    @Generated
+    public ServiceOrderServiceMessagesPanel(UserSession userSession, ServiceOrderBoundaryService serviceOrderService,
+            ServiceMessageBoundaryService serviceMessageService) {
+        this.userSession = userSession;
+        this.serviceOrderService = serviceOrderService;
+        this.serviceMessageService = serviceMessageService;
+    }
+
+    /**
+     * @return the list of elements
+     */
+    @Generated
+    public Collection<ServiceOrderServiceMessagesDTO> getServiceMessagesList() {
+        return serviceMessagesList;
+    }
+
+    /**
+     * @return the selected item
+     */
+    @Generated
+    public ServiceOrderServiceMessagesDTO getSelItemOfServiceMessages() {
+        return selItemOfServiceMessages;
+    }
+
+    /**
+     * @param selItemOfServiceMessages
+     */
+    @Generated
+    public void setSelItemOfServiceMessages(ServiceOrderServiceMessagesDTO selItemOfServiceMessages) {
+        this.selItemOfServiceMessages = selItemOfServiceMessages;
+    }
+
+    /**
+     * @return the URL of the page this grid panel is included in
+     */
+    @Generated
+    public String getCurrentPageURL() {
+        return currentPageURL;
+    }
+
+    /**
+     * @param currentPageURL
+     */
+    @Generated
+    public void setCurrentPageURL(String currentPageURL) {
+        this.currentPageURL = currentPageURL;
+    }
+
+    /**
+     * @return the ID of the selected parent object
+     */
+    @Generated
+    public String getSelectedObjectId() {
+        return selectedObjectId;
+    }
+
+    /**
+     * @param selectedObjectId
+     */
+    @Generated
+    public void setSelectedObjectId(String selectedObjectId) {
+        this.selectedObjectId = selectedObjectId;
+    }
+
+    /**
+     * @return true if the panel is in read-only mode
+     */
+    @Generated
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    /**
+     * @param readOnly
+     */
+    @Generated
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+
+    /**
+     * Event that will be fired if user performs a double-click on a grid row
+     */
+    @Generated
+    public void onServiceMessagesGridDoubleClick() {
+        logger.debug("Handle double-click event");
+
+        userSession.redirectTo(getCurrentPageURL(), openViewServiceMessageDialog());
+    }
+
+    /**
+     * Delete selected element
+     */
+    @Generated
+    public void deleteServiceMessage() {
+        try {
+            logger.debug("Delete selected object with id '{}'", selItemOfServiceMessages.getId());
+
+            serviceMessageService.deleteServiceMessage(selItemOfServiceMessages.getId());
+        }
+        catch (final Exception e) {
+            logger.error("Error while deleting selected object!", e);
+
+            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_ERROR, OPERATION_DELETE_FAIL, e);
+        }
+
+        fetchServiceMessages();
+    }
+
+    /**
+     * Create copy of selected element
+     * @return the navigation target
+     */
+    @Generated
+    public String copy() {
+        try {
+            logger.debug("Create a copy of the selected object with id '{}'", selItemOfServiceMessages.getId());
+
+            serviceMessageService.copy(selItemOfServiceMessages.getId(), userSession.getPrincipal().getId());
+        }
+        catch (final Exception e) {
+            logger.error("Error while creating a copy of the selected object!", e);
+
+            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_ERROR, OPERATION_COPY_FAIL, e);
+            return "";
+        }
+
+        fetchServiceMessages();
+        return "";
+    }
+
+    /**
+     * Open dialog
+     * @return the navigation target
+     */
+    @Generated
+    public String openViewServiceMessageDialog() {
+        var url = "";
+
+        if (userSession.checkAuthorization(false, ROLE_ADMINISTRATOR, ROLE_READONLY))
+            url = ViewServiceMessageDialog.PAGE_INIT_URL + selItemOfServiceMessages.getId();
+
+        return url;
+    }
+
+    /**
+     * Initialize view
+     */
+    @Generated
+    public void initView() {
+        logger.debug("Initialize grid panel");
+
+        bundle = ResourceBundle.getBundle(DEFAULT_BUNDLE_NAME, userSession.getLocale());
+
+        fetchServiceMessages();
+
+        logger.debug("Grid panel initialization finished");
+    }
+
+    /**
+     * Fetch data for grid panel
+     */
+    @Generated
+    public void fetchServiceMessages() {
+        // Get data from server
+        try {
+            serviceMessagesList = new ArrayList<>(serviceOrderService.getServiceMessagesOfServiceOrder(selectedObjectId));
+        }
+        catch (final Exception e) {
+            logger.error("Error while fetching grid panel data for object with id '{}'!", selectedObjectId, e);
+
+            MessageUtil.sendFacesMessage(bundle, FacesMessage.SEVERITY_ERROR, DIALOG_INIT_FAIL, e);
+        }
+    }
+
+}
